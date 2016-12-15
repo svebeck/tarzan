48ea36dc20c20b29739547b11d0834051ef12246
Stefan Svebeck <svebeck@gmail.com>
2016-12-15 13:47:20 +0100

    testing new materials

---
+++ Tarzan/Assets/scripts/LightController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour {
+
+    public Transform target;
+    public Light sun;
+    public Camera camera;
+
+    public Color sunLight;
+    public Color caveLight;
+
+    public Color daySkyColor;
+    public Color caveSkyColor;
+
+    public float sunIntensityStart = 1.6f;
+    public float sunIntensityDeep = 0.3f;
+
+    void Update()
+    {
+        float depthFactor = -target.position.y / 50f;
+
+        sun.intensity = Mathf.Lerp(sunIntensityStart, sunIntensityDeep, depthFactor);
+        sun.color = Color.Lerp(sunLight, caveLight, depthFactor);
+        camera.backgroundColor = Color.Lerp(daySkyColor, caveSkyColor, depthFactor);
+    }
+}
